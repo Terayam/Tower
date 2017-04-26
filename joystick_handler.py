878c30bcd9df2cd451555c86c1d79ac7f34de5c0
terayam <terayam@gmail.com>
2017-04-25 20:36:43 -0500

    Added basic analog stick handling added

--- joystick_handler.py
+++ joystick_handler.py
@@ -18,28 +18,49 @@ class Joystick_handler():
 
         if(self.joystick):
 
+            moveLeftHat = False
+            moveRightHat = False
+            moveUpHat = False
+            moveDownHat = False
+
+            moveLeftAna = False
+            moveRightAna = False
+            moveUpAna = False
+            moveDownAna = False
+
             # Check hat X position
             if(self.joystick.hat_x > 0):
-                self.moveLeft = False
-                self.moveRight = True
+                moveLeftHat = False
+                moveRightHat = True
             elif(self.joystick.hat_x < 0):
-                self.moveLeft = True
-                self.moveRight = False
-            else:
-                self.moveLeft = False
-                self.moveRight = False
+                moveLeftHat = True
+                moveRightHat = False
 
             # check hat Y position
             if(self.joystick.hat_y > 0):
-                self.moveUp = True
-                self.moveDown = False
+                moveUpHat = True
+                moveDownHat = False
             elif(self.joystick.hat_y < 0):
-                self.moveUp = False
-                self.moveDown = True
-            else:
-                self.moveUp = False
-                self.moveDown = False
+                moveUpHat = False
+                moveDownHat = True
 
             # Check X axis position
+            if(self.joystick.x < -0.5):
+                moveLeftAna = True
+                moveRightAna = False
+            elif(self.joystick.x > 0.5):
+                moveLeftAna = False
+                moveRightAna = True
 
             # Check y axis position
+            if(self.joystick.y < -0.5):
+                moveUpAna = True
+                moveDownAna = False
+            elif(self.joystick.y > 0.5):
+                moveUpAna = False
+                moveDownAna = True
+
+            self.moveLeft = moveLeftHat or moveLeftAna
+            self.moveRight = moveRightHat or moveRightAna
+            self.moveUp = moveUpHat or moveUpAna
+            self.moveDown = moveDownHat or moveDownAna
