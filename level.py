53ea66d812f744e08e3191e50e2af534953eb850
terayam <terayam@gmail.com>
2018-07-22 13:04:46 -0500

    Organized level creation into two functions

--- level.py
+++ level.py
@@ -7,13 +7,21 @@ class Level:
     def __init__(self, sprite_batch):
 
         self.background = None
-        self.has_player = False
         self.sprite_batch = sprite_batch
         self.entities = []
+
+        # Create all level objects
+        self.build_level()
+
+    def build_level(self):
+        self.add_walls()
+        self.add_entities()
 
     def set_background(self, filename):
         self.background = pyglet.image.load(filename)
 
+    # TODO: This seems like it shouldn't really be a member function of the
+    # Level class.  This is something a LevelBuilder should do instead
     def add_wall(self, wallX, wallY, wallW, wallH):
 
         # Create a new wall to add to the list
--- levels.py
+++ levels.py
@@ -9,14 +9,19 @@ class Debug1(level.Level):
         # Call the base class initializer
         super(Debug1, self).__init__(*args, **kwargs)
 
-        self.has_player = True
         self.set_background("img/bg.png")
+
+    def add_walls(self):
+
         self.add_wall(250, 200, 50, 200)
         self.add_wall(350, 200, 50, 200)
         self.add_wall(175, 150, 300, 10)
 
+    def add_entities(self):
+
         test_enemy = debt.Debt('img/enemy.png', batch=self.sprite_batch)
         self.entities.append(test_enemy)
+
 
 class Debug2(level.Level):
 
