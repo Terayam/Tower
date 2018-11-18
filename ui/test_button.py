c3ed376a7d3f2a48ffdafc91d961c994800acf2d
terayam <terayam@gmail.com>
2018-11-18 12:30:00 -0600

    Corrected the test button changing to the release state when the click happened outside of the bbox

--- ui/test_button.py
+++ ui/test_button.py
@@ -3,6 +3,13 @@ from ui import ui_element
 
 class Test_Button(ui_element.Ui_element):
 
+    def __init__(self, *args, **kwargs):
+        # Call the base class initializer
+        super(Test_Button, self).__init__(*args, **kwargs)
+
+        # Button was clicked
+        self.was_clicked = False
+
     #####################
     # Behavior functions
     #####################
@@ -13,7 +20,12 @@ class Test_Button(ui_element.Ui_element):
         self.set_clip(0)
 
     def clicked(self, button):
+
+        self.was_clicked = True
         self.set_clip(2)
 
     def unclicked(self, button):
-        self.set_clip(1)
+
+        if(self.was_clicked):
+            self.was_clicked = False
+            self.set_clip(1)
